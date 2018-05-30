7616a22bd63c501e47895f2f8cae59b707c7832b
Ilya Kreymer <ikreymer@gmail.com>
2018-05-30 12:39:12 -0700

    announce: add maintainance downtime widget

--- webrecorder/webrecorder/static/user.js
+++ webrecorder/webrecorder/static/user.js
@@ -10,6 +10,15 @@ $(function(){
       $('#news-alert').hide();
     }
 
+    $('#news-alert-top').on('close.bs.alert', function () {
+        setStorage("__wr_skipMt", "1");
+    });
+
+    if(getStorage("__wr_skipMt") === "1") {
+      $('#news-alert-top').hide();
+    }
+
+
     $('#create-modal').on('shown.bs.modal', function () {
         $('#title').select();
     });
